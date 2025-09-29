a56a2aa70848cd993cd5a1921912e402019a9c7a
arockiaprabhaker-dot-com <arockiaprabhaker@gmail.com>
2025-09-29 19:24:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-print("Hello DevOps!")
-# trigger build
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello DevOps!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)  # Important: host=0.0.0.0
